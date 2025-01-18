09ad910636ceba0bd2d187a2ebcddf088f14f3fb
Alexis Sushko <alexis.sushko@gmail.com>
2025-01-18 17:52:39 -0500

    Added route to Auth Form for logging in

--- src/router.jsx
+++ src/router.jsx
@@ -1,12 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import Root from "./layout/Root";
+import AuthForm from "./components/auth/AuthForm";
+import Login from "./components/auth/AuthForm";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    children: [{ index: true, element: <HomePage /> }],
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "/auth", element: <AuthForm /> },
+      { path: "/users/login", element: <Login /> },
+    ],
   },
 ]);
 
